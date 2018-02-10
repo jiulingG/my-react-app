49aa7bfd6d1ff3d3e911b26bda36222dcc7666fa
Unknown <jiulinggao@gmail.com>
2018-02-10 19:35:48 +1100

    test push in gitkraken

--- src/component/Dreamspage.js
+++ src/component/Dreamspage.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import Appbar from './Appbar.js';
 
-const Dreamspage = () =>{ 
-	return(<div align='center'>
-		<h1>Dreams</h1>
+const Dreamspage = () =>(
+	<div align='center'>
+		<h1>Dreamasdfsdfwfe</h1>
 	     <Appbar />
 	</div>
 
 );
-}
+
 
 
 export default Dreamspage; 
